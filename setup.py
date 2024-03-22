e2c49728586518dd59b1d7596d34d3eb47ac6381
Vardonir@portable potato <135014510+vardonir@users.noreply.github.com>
2024-03-22 10:41:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="pyDXHR",
     version="1.0.0",
-    description="A Python library for reading and writing Deus Ex: Human Revolution files",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
