4a7697e4747e32cd9787f4b869eb77bd3f5a72a6
Vardonir@portable potato <135014510+vardonir@users.noreply.github.com>
2024-03-22 10:46:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name="pyDXHR",
     version="1.0.0",
     packages=find_packages(),
+    description="Collection of tools for working with Deus Ex: Human Revolution's game files",
+    long_description="See pydxhr.vardonir.com for more information",
+    author="Vardonir",
     entry_points={
         "console_scripts": [
             "bigfile = pyDXHR.entrypoints:bigfile",
